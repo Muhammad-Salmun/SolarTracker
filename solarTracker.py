09b111578ff8f53078b522a66a3f7cb5ac3a20ca
Muhammad-Salmun <muhammedsalmun@gmail.com>
2022-03-16 23:58:29 +0530

    found optimal points of placing pistons

--- solarTracker.py
+++ solarTracker.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,6 +24,12 @@ def equation_plane(x1, y1, z1, x2, y2, z2, x3, y3, z3):
 #Function to find equation of a line.
 #def length_line():
 
+#Function to plot a point with cordinate 
+def plotPoint(x:float,y:float,z:float,name:str = nullcontext,pointColor:str = 'black'):
+    ax.scatter(x,y,z,c=pointColor,s=10)
+    text = str(name) + str('(') + str(x) + ', ' + str(y) + ', ' + str(z) + str(')') 
+    ax.text(x, y, z, text, zdir=(1, 1, 1))
+
 
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
@@ -32,15 +39,15 @@ y = np.linspace(0, 20, 100)
 
 x, y = np.meshgrid(x, y)
 
-#points on the plane.
-xp1,yp1,zp1 = 3,5,35
-xp2,yp2,zp2 = 13,5,35
-xp3,yp3,zp3 = 8,8.5,35
-
 #points on the ground.
-xg1,yg1,zg1 = 3,5,0
-xg2,yg2,zg2 = 13,5,0
-xg3,yg3,zg3 = 8,8.5,0
+xg1,yg1,zg1 = 7.825,7.5,0
+xg2,yg2,zg2 = 10,12.175,0
+xg3,yg3,zg3 = 12.5,7.5,0
+
+#points on the plane.
+xp1,yp1,zp1 = 9.167,8.94,2
+xp2,yp2,zp2 = 10,10.373,2
+xp3,yp3,zp3 = 10.83,8.94,2
 
 xg = [xg1,xg2,xg3]
 yg = [yg1,yg2,yg3]
@@ -66,13 +73,24 @@ ax = fig.gca(projection='3d')
 
 #plot the plane
 ax.plot_surface(x, y, eq)
-#plot the points
-ax.scatter(xg1,yg1,zg1,c='red',s=10)
-ax.scatter(xp1,yp1,zp1,c='green',s=10)
+
+#plot the points on ground
+plotPoint(xg1,yg1,zg1,'Pg1','red')
+plotPoint(xg2,yg2,zg2,'Pg2','red')
+plotPoint(xg3,yg3,zg3,'Pg3','red')
+
+#show points on plane
+plotPoint(xp1,yp1,zp1,'Pp1','black')
+plotPoint(xp2,yp2,zp2,'Pp2','black')
+plotPoint(xp3,yp3,zp3,'Pp3','black')
 
 #plot the lines
 ax.plot(xl1,yl1,zl1,c='black')
 ax.plot(xl2,yl2,zl2,c='black')
 ax.plot(xl3,yl3,zl3,c='black')
+
+ax.set_xlabel("x axis")
+ax.set_ylabel("y axis")
+ax.set_zlabel("z axis")
 
 plt.show()
