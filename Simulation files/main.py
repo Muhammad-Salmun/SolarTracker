167e30a03e4fc75a2c88a5476b43f2afb9fa0ec4
Muhammad-Salmun <muhammedsalmun@gmail.com>
2022-05-15 01:11:19 +0530

    trying to make multiple matrix

---
+++ Simulation files/main.py
@@ -0,0 +1,21 @@
+from lineDistance import distance
+from pointsOnTriangle import pointsOfTriangle
+import angleOfPlane
+from setPistonHeight import pistonPlanePosition
+
+#calculating points of contact on ground
+xg,yg = pointsOfTriangle(10,10,5)
+zg = [0,0,0]
+
+#calculating points of contact of panel
+xp,yp,zp = pistonPlanePosition(10,10,10,0.5)
+
+#calculating angle of plane 
+angleXY = angleOfPlane.angleOfPlaneXY(xp,yp,zp)
+print("angle of plane wrt xy plane is", angleXY)
+
+angleYZ = angleOfPlane.angleOfPlaneXY(xp,yp,zp)
+print("angle of plane wrt yz plane is", angleYZ)
+
+angleXZ = angleOfPlane.angleOfPlaneXY(xp,yp,zp)
+print("angle of plane wrt xz plane is", angleXZ)
--- Simulation files/plotPoint.py
+++ Simulation files/plotPoint.py
@@ -2,4 +2,4 @@
 def plotPoint(ax, x:float, y:float, z:float, name:str, pointColor:str = 'black'):
     ax.scatter(x,y,z,c=pointColor,s=10)
     text = str(name) + str('(') + str(x) + ', ' + str(y) + ', ' + str(z) + str(')') 
-    ax.text(x, y, z, text, zdir=( 1, 1, 1))
+    #ax.text(x, y, z, text, zdir=( 1, 1, 1))
