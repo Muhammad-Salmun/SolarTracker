5ece278ab93f4eae5e236a62b9e6453d84eb647e
Muhammad-Salmun <muhammedsalmun@gmail.com>
2022-06-19 12:53:24 +0530

    made iterable

--- Simulation files/rotatingPanel.py
+++ Simulation files/rotatingPanel.py
@@ -9,10 +9,22 @@ finalArray = []
 i = 0
 
 def rotationalArray(pos):
-    for i in range(0,3):
-        finalArray = [[1,i,0],[i,1,i],[i,i,1]]
-        rotationalArray.append(finalArray)
-    return finalArray[pos]
+    # for i in range(0,pos):
+    #     finalArray = [[1,i,0],[i,1,i],[i,i,1]]
+    #     rotationArray.append(finalArray)
+    pos = pos*-0.1
+    finalArray = [[ 1,       0,    0],
+                  [ 0,     1,      0],
+                  [ pos,     pos,    1]]
+    return finalArray
 
-array = rotationalArray(3)
-print(array)
+def rotationalArray_sim(pos):
+    # for i in range(0,pos):
+    #     finalArray = [[1,i,0],[i,1,i],[i,i,1]]
+    #     rotationArray.append(finalArray)
+    finalArray = [[ 1,       0,    0],
+                  [ 0,     1,      0],
+                  [ 0,     -pos,    1]]
+    return finalArray
+# array = rotationalArray(3)
+# print(array)
